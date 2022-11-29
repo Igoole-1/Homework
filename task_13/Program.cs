a928f4bd03e9d4f0f4571f8b6845b23392591f3d
Aigul <igoole.g@mail.ru>
2022-11-29 23:57:41 +0300

    file added

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine ("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int l=0;
+if (a>99)
+{
+for (l=0; b>0; l++)
+{
+   b = (b / 10); 
+}
+//Console.WriteLine(l);
+//Console.WriteLine(b);
+//Console.WriteLine(a);
+
+while (l>3)
+{
+  l = (l-1);
+  a = (a/10);
+  //Console.WriteLine(l);
+  //Console.WriteLine(a);
+}
+Console.WriteLine(a%10);
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
