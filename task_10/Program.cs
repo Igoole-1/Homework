5e88a52c5457e58c3541fac713270e539ba1cb5b
Aigul <igoole.g@mail.ru>
2022-11-29 23:38:41 +0300

    file added

---
+++ task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.WriteLine ("Введите число от 100 до 1000");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int a1 = a / 10;
+//Console.WriteLine(a1);
+
+int a3 = a1 % 10;
+//Console.WriteLine(a3);
+
+int b = a1*10 + a3;
+//Console.WriteLine(b);
+
+int a2 = b % 10;
+Console.WriteLine(a2);
